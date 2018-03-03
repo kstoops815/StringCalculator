c503dae8d1d4d35aa3ede59bd40b64d32da8620c
Kelly Stoops <kstoops815@gmail.com>
2018-03-03 10:55:03 -0600

    first test case working

---
+++ StringCalculator/NumberAdder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class NumberAdder
+    {
+        public int AddNumbers(string number)
+        {
+            if(number == "")
+            {
+                return 0;
+            }
+            return 0;
+        }
+        
+    }
+}
---
+++ StringCalculatorTests/AddNumbersTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    public class AddNumbersTests
+    {
+        [Test]
+        public void Entering_an_empty_string_results_in_the_sum_of_0()
+        {
+            //Arrange
+            var number = "";
+            var numberAdder = new NumberAdder();
+
+            //Act
+            var total = numberAdder.AddNumbers(number);
+
+            //Assert
+            Assert.AreEqual(0, total);
+        }
+
+        [Test]
+        public void Entering_one_number_results_in_returning_the_same_number()
+        {
+
+        }
+    }
+}
+
+        
+
+
+  
+
+
+
